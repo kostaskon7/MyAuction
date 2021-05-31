f6042f28aae947f9c6a45897ed366b97666ba803
kostaskon7 <kostaskon7@gmail.com>
2021-05-31 19:09:39 +0300

    Updates

---
+++ precache-manifest.2555b97511780ebc83ad35371db09be2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b654f3a2e5ba93f286fc00cbe8ad0d12",
+    "url": "/MyAuction/index.html"
+  },
+  {
+    "revision": "534c6b2f4fb57397f653",
+    "url": "/MyAuction/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "aa908802f5111d81b0bb",
+    "url": "/MyAuction/static/js/2.246cd8a8.chunk.js"
+  },
+  {
+    "revision": "534c6b2f4fb57397f653",
+    "url": "/MyAuction/static/js/main.59009552.chunk.js"
+  },
+  {
+    "revision": "2d1ee58013822c75bbd1",
+    "url": "/MyAuction/static/js/runtime-main.03a03182.js"
+  }
+]);
